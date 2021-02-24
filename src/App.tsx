db32ca5288b7d63b2ec00ebc67edce6eae918be1
djdeo <deoseoer@gmail.com>
2021-02-24 16:30:38 +0800

    feat: vite+ant-design

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </Button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
